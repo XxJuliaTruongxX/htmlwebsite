149cc59f071e1dc484ade171b02c88c51ff0d4b0
XxMelloettaxX <juliaTruong75@gmail.com>
2023-01-10 23:43:18 -0600

    Covert buttons to "a" tags and updated github/linkedin urls
    Co-authored-by: Samyok Nepal <sam@yok.dev>

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 function animation() {}
+
+
+const divs = document.querySelectorAll(".changingwidth");
+for(let i = 0; i < divs.length; i++){
+    const borderDiv = divs[i]; 
+    borderDiv.style.width = "25%";
+}
